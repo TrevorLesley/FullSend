337df2495cf3aaefc99e7e57424aea769d2af8a7
Christina <Christinepeninsula@hotmail.com>
2020-10-24 00:49:14 -0400

    commit

---
+++ first_function.py
@@ -0,0 +1,11 @@
+import random
+
+def function():
+    bangerz = ["As Much As I Ever Could", "Same Road", "Long Way", "Balloons",
+                "Music Is Math", "Michicant", "Return of The Dreamer"]
+
+    shuffle = random.choice(bangerz) 
+
+    print("This current banger is;" shuffle)
+
+function()
